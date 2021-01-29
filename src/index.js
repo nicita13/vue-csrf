b94c46806c72d11f6b74a9b21f411afca35fa340
Mikita Amialkovič <nikita131313@gmail.com>
2021-01-29 12:34:15 +0300

    vue 3 changes

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ const CsrfReader = require('./CsrfReader');
 const Csrf = require('./Csrf');
 
 export default {
-  install(Vue, options = {
+  install(app, options = {
     attribute: 'content',
     selector: 'meta[name="csrf-token"]',
   }) {
     let csrfReader = new CsrfReader(options.selector, options.attribute);
     let csrf = new Csrf(csrfReader.getToken());
 
-    Vue.prototype.$csrf = csrf;
-    Vue.csrf = csrf;
+    app.config.globalProperties.prototype.$csrf = csrf;
 
-    Object.defineProperty(Vue.prototype, '$csrfToken', {get: () => csrf.get()});
-    Object.defineProperty(Vue, 'csrfToken', {get: () => csrf.get()});
+    Object.defineProperty(app.config.globalProperties, '$csrfToken', {get: () => csrf.get()});
   },
 };
